e3583803728c72eba363c267f07ba568a2303a7e
weijian <303101610@qq.com>
2021-08-17 14:05:19 +0800

    tidy up the code of tcp server

--- app/lib/tcp_server.dart
+++ app/lib/tcp_server.dart
@@ -10,7 +10,7 @@ class _TcpServer {
 
   Map<Socket, List<int>> _clients = new Map();
 
-  final messages = <Message>[];
+  final _messages = <Message>[];
 
   //私有构造函数
   _TcpServer._internal();
@@ -35,161 +35,176 @@ class _TcpServer {
         isStarted = true;
         bus.fire(new TcpServerEvent(
             "Server[${_server.address.host}:${_server.port}] startup"));
+      }).catchError((e) {
+        print("the tcp server catch error: $e");
+        _restart(host, port);
+        return;
       });
-
-      // 监听TCP服务器socket，等待accept
-      await _server.listen((Socket client) async {
-        client.setOption(SocketOption.tcpNoDelay, true);
-        _clients[client] ??= List.generate(0, (index) => 0);
-        bus.fire(new TcpServerEvent(
-            "Client[${client.remoteAddress.host}:${client.remotePort}] connect to server[${_server.address.host}:${_server.port}]"));
-
-        try {
-          // 监听客户端socket
-          await client.listen((Uint8List data) async {
-            // 接收到客户端发来的数据data
-            var recvData = _clients[client];
-            int orgRecvDataBytes = recvData!.length;
-            recvData.addAll(data);
-
-            while (true) {
-              int recvDataBytes = recvData.length;
-              if (recvDataBytes <= 2) {
-                break;
-              }
-
-              ByteData bd = Uint8List.fromList(recvData)
-                  .buffer
-                  .asByteData(0, recvDataBytes);
-
-              // 一个包的长度, 1Byte
-              int packetBytes = bd.getUint8(0);
-              if (packetBytes > recvDataBytes) {
-                recvData.removeRange(orgRecvDataBytes, data.lengthInBytes);
-                print(
-                    "Client[${client.remoteAddress.host}:${client.remotePort}] receive data error, ignore received data");
-                break;
-              }
-
-              int opt = bd.getUint8(1);
-              if (opt >= OperationType.opt_max.index ||
-                  opt < OperationType.opt_echo.index) {
-                recvData.removeRange(orgRecvDataBytes, data.lengthInBytes);
-                print(
-                    "Client[${client.remoteAddress.host}:${client.remotePort}] receive data error, operationType=$opt, ignore received data");
-                break;
-              }
-
-              // 包的操作类型, 1Byte
-              OperationType optType = OperationType.values[opt];
-
-              switch (optType) {
-                case OperationType.opt_echo:
-                  {
-                    String echo = String.fromCharCodes(
-                        Uint8List.sublistView(data, 2, packetBytes));
-                    Message msg = new EchoMessage(echo);
-                    messages.add(msg);
-                    recvData.removeRange(0, packetBytes);
-                    // print(msg.toString());
-                  }
-                  break;
-                case OperationType.opt_ack:
-                  {
-                    int ack = bd.getUint8(2);
-                    Message msg = new AckMessage(ack);
-                    messages.add(msg);
-                    recvData.removeRange(0, packetBytes);
-                    print(msg.toString());
-                  }
-                  break;
-                case OperationType.opt_car_status:
-                  {
-                    int angel = bd.getUint16(2);
-                    MotorRotatingLevel level =
-                        bd.getUint8(4) as MotorRotatingLevel;
-                    Message msg = new CarStatusMessage(angel, level);
-                    messages.add(msg);
-                    recvData.removeRange(0, packetBytes);
-                    print(msg.toString());
-                  }
-                  break;
-                default:
-                  {
-                    print(
-                        "Client[${client.remoteAddress.host}:${client.remotePort}] parse data error");
-                  }
-              }
-            }
-
-            for (var msg in messages) {
-              switch (msg.optType) {
-                case OperationType.opt_echo:
-                  {
-                    _send(client, msg);
-                  }
-                  break;
-                case OperationType.opt_ack:
-                  {}
-                  break;
-                case OperationType.opt_car_status:
-                  {}
-                  break;
-                default:
-                  {
-                    print(
-                        "Client[${client.remoteAddress.host}:${client.remotePort}] handle message error");
-                  }
-              }
-            }
-
-            print(
-                "Client[${client.remoteAddress.host}:${client.remotePort}] message handle done, _recvData.len=${recvData.length}, recvData=$recvData");
-            await client.flush();
-            messages.clear();
-          }, onError: (error) async {
-            // 监听客户端socket错误
-            print(
-                "Client[${client.remoteAddress.host}:${client.remotePort}] onError, $error");
-            await client.close();
-            client.close();
-            _clients.remove(client);
-          }, onDone: () {
-            print(
-                "Client[${client.remoteAddress.host}:${client.remotePort}] onDone");
-          }, cancelOnError: false);
-        } catch (e) {
-          print(
-              "Client[${client.remoteAddress.host}:${client.remotePort}] receive data error，$e");
-          _clients.remove(client);
-          client.close();
-        }
-      }, onError: (error) {
-        // TCP服务器socket监听错误
-        print(
-            "Server[${_server.address.host}:${_server.port}] accept error, $error");
-        for (var c in _clients.entries) {
-          c.key.close();
-        }
-        _clients.clear();
-        _server.close();
-        isStarted = false;
-      }, onDone: null, cancelOnError: false);
     } catch (e) {
-      print("连接socket出现异常，e=${e.toString()}");
-      for (var c in _clients.entries) {
-        c.key.close();
-      }
-      _clients.clear();
-      _server.close();
-      isStarted = false;
+      print("the tcp server bind error: $e");
+      _restart(host, port);
+      return;
     }
+
+    // 监听TCP服务器socket，等待accept
+    await _server.listen((Socket client) async {
+      client.setOption(SocketOption.tcpNoDelay, true);
+      _clients[client] ??= List.generate(0, (index) => 0);
+      bus.fire(new TcpServerEvent(
+          "Client[${client.remoteAddress.host}:${client.remotePort}] connect to server[${_server.address.host}:${_server.port}]"));
+
+      try {
+        _clientListen(client);
+      } catch (e) {
+        print(
+            "Client[${client.remoteAddress.host}:${client.remotePort}] listen error，$e");
+        _closeClient(client);
+      }
+    }, onError: (e) {
+      // TCP服务器socket监听错误
+      print("Server[${_server.address.host}:${_server.port}] listen error, $e");
+      _restart(host, port);
+    }, onDone: null, cancelOnError: false);
+  }
+
+  void _restart(String host, int port) async {
+    for (var c in _clients.entries) {
+      await c.key.close();
+    }
+
+    _clients.clear();
+    await _server.close();
+    isStarted = false;
+  }
+
+  void _clientListen(Socket client) async {
+    // 监听客户端socket
+    await client.listen((Uint8List data) async {
+      // 接收到客户端发来的数据data
+      var recvData = _clients[client];
+      int orgRecvDataBytes = recvData!.length;
+      recvData.addAll(data);
+
+      while (true) {
+        int recvDataBytes = recvData.length;
+        if (recvDataBytes <= 2) {
+          break;
+        }
+
+        ByteData bd =
+            Uint8List.fromList(recvData).buffer.asByteData(0, recvDataBytes);
+
+        // 一个包的长度, 1Byte
+        int packetBytes = bd.getUint8(0);
+        if (packetBytes > recvDataBytes) {
+          recvData.removeRange(orgRecvDataBytes, data.lengthInBytes);
+          print(
+              "Client[${client.remoteAddress.host}:${client.remotePort}] receive data error, ignore received data");
+          break;
+        }
+
+        int opt = bd.getUint8(1);
+        if (opt >= OperationType.opt_max.index ||
+            opt < OperationType.opt_echo.index) {
+          recvData.removeRange(orgRecvDataBytes, data.lengthInBytes);
+          print(
+              "Client[${client.remoteAddress.host}:${client.remotePort}] receive data error, operationType=$opt, ignore received data");
+          break;
+        }
+
+        // 包的操作类型, 1Byte
+        OperationType optType = OperationType.values[opt];
+
+        switch (optType) {
+          case OperationType.opt_echo:
+            {
+              String echo = String.fromCharCodes(
+                  Uint8List.sublistView(data, 2, packetBytes));
+              Message msg = new EchoMessage(echo);
+              _messages.add(msg);
+              recvData.removeRange(0, packetBytes);
+              // print(msg.toString());
+            }
+            break;
+          case OperationType.opt_ack:
+            {
+              int ack = bd.getUint8(2);
+              Message msg = new AckMessage(ack);
+              _messages.add(msg);
+              recvData.removeRange(0, packetBytes);
+              print(msg.toString());
+            }
+            break;
+          case OperationType.opt_car_status:
+            {
+              int angel = bd.getUint16(2);
+              MotorRotatingLevel level = bd.getUint8(4) as MotorRotatingLevel;
+              Message msg = new CarStatusMessage(angel, level);
+              _messages.add(msg);
+              recvData.removeRange(0, packetBytes);
+              print(msg.toString());
+            }
+            break;
+          default:
+            {
+              print(
+                  "Client[${client.remoteAddress.host}:${client.remotePort}] parse data error");
+            }
+        }
+      }
+
+      for (var msg in _messages) {
+        switch (msg.optType) {
+          case OperationType.opt_echo:
+            {
+              _send(client, msg);
+            }
+            break;
+          case OperationType.opt_ack:
+            {}
+            break;
+          case OperationType.opt_car_status:
+            {}
+            break;
+          default:
+            {
+              print(
+                  "Client[${client.remoteAddress.host}:${client.remotePort}] handle message error");
+            }
+        }
+      }
+
+      print(
+          "Client[${client.remoteAddress.host}:${client.remotePort}] message handle done, _recvData.len=${recvData.length}, recvData=$recvData");
+      await client.flush();
+      _messages.clear();
+    }, onError: (error) async {
+      // 监听客户端socket错误
+      print(
+          "Client[${client.remoteAddress.host}:${client.remotePort}] onError, $error");
+      await client.close();
+      _closeClient(client);
+      return;
+    }, onDone: () {
+      print("Client[${client.remoteAddress.host}:${client.remotePort}] onDone");
+    }, cancelOnError: false);
+  }
+
+  void _closeClient(Socket client) async {
+    _clients.remove(client);
+    await client.close();
   }
 
   void _send(Socket client, Message msg) async {
     if (!isStarted) {
       return;
     }
+
+    client.drain().then((value) async {
+      print('client[${client.remoteAddress.host}:${client.remotePort}] closed');
+      _closeClient(client);
+      return;
+    });
 
     try {
       client.add(msg.encode());
@@ -209,6 +224,12 @@ class _TcpServer {
     try {
       for (var client in _clients.entries) {
         var c = client.key;
+        c.drain().then((value) async {
+          print('client[${c.remoteAddress.host}:${c.remotePort}] closed');
+          _closeClient(c);
+          return;
+        });
+
         c.add(msg.encode());
         await c.flush();
         print(
